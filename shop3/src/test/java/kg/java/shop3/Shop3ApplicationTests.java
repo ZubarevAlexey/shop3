1743e480acb1fcff5231322a7c5af1b0b0e94bcb
User <stparchkg@gmail.com>
2023-10-19 16:43:33 +0600

    setup new spring project

---
+++ shop3/src/test/java/kg/java/shop3/Shop3ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.java.shop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
